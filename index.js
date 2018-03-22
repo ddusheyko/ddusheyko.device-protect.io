4a34c1bae5d828bbdac2324251ec6eff8231d0f8
Dmitriy Dusheyko <ddushey@gmail.com>
2018-03-22 13:50:44 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 $(function() {
   // Initialize collapse button
-  $('.button-collapse').sideNav();
+  $("[data-activates='slide-out']").sideNav();
   // Initialize collapsible (uncomment the line below if you use the dropdown variation)
   //$('.collapsible').collapsible();
+  var prices = {
+    st_m: '7.99',
+    st_2y: '99.00',
+    v_total: '9.00',
+    v_decline: '0.00',
+    v_extended: '3.00',
+    v_coach: '7.00',
+    v_mobile_no_florida: '11.00',
+    v_mobile_florida: '11.00',
+    v_wireless: '7.15',
+    apple: '129.00',
+    none: '0.00',
+  };
+
+  $("input[name='plan']").change(function() {
+    let key = $(this).attr('id');
+    $('.total').text(prices[key]);
+  });
+
+  /*$(".collapsible-item").click(function() {
+    $('.expandable').text('expand_less');
+  });*/
+  $(".collapsible-header").click(function() {
+    var was_active = $(this).hasClass('active');
+    $('.expandable').text(was_active ? 'add': 'remove');
+  });
 });
